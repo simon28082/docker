1ee4e2def7400ca36932c3f93a4cdba608756075
crcms <crcms@crcms.cn>
2019-01-01 22:20:50 +0800

    Add config build

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -2,6 +2,9 @@
 
 PHP_INI_CONFIG_PATH=${PHP_FPM_CONFIG}/php.ini
 PHP_FPM_CONFIG_PATH=${PHP_FPM_CONFIG}/php-fpm.conf
+PHP_INI_CONFIG_RUN_PATH=${PHP_FPM_CONFIG}/php-run.ini
+PHP_FPM_CONFIG_RUN_PATH=${PHP_FPM_CONFIG}/php-fpm-run.conf
+
 
 if [ ! -f "${PHP_INI_CONFIG_PATH}" ]; then
     PHP_INI_CONFIG_PATH=/usr/local/etc/php/php.ini-production
@@ -11,10 +14,23 @@ if [ ! -f "${PHP_FPM_CONFIG_PATH}" ]; then
     PHP_FPM_CONFIG_PATH=/usr/local/etc/php-fpm.conf
 fi
 
+# chown
 chown ${APP_RUN_PUID}:${APP_RUN_PGID} -R ${CONTAINER_CODE_PATH}
 
+# build
+
+cat "${PHP_INI_CONFIG_PATH}" \
+| sed "s#\${APP_RUN_NAME}#${APP_RUN_NAME}#g" \
+| sed "s#\${APP_RUN_GROUP}#${APP_RUN_GROUP}#g" \
+> "${PHP_INI_CONFIG_RUN_PATH}"
+
+cat "${PHP_FPM_CONFIG_PATH}" \
+| sed "s#\${APP_RUN_NAME}#${APP_RUN_NAME}#g" \
+| sed "s#\${APP_RUN_GROUP}#${APP_RUN_GROUP}#g" \
+> "${PHP_FPM_CONFIG_RUN_PATH}"
+
 # run
-php-fpm -c ${PHP_INI_CONFIG_PATH} -y ${PHP_FPM_CONFIG_PATH}
+php-fpm -c ${PHP_INI_CONFIG_RUN_PATH} -y ${PHP_FPM_CONFIG_RUN_PATH}
 
 # clean opcache
 php -r 'if(function_exists("opcache_reset")) {opcache_reset();}'
