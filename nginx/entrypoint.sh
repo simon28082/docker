70af7f30f3e6faa934f8b7f493a6bf6faa9cda6d
crcms <crcms@crcms.cn>
2019-01-01 19:05:31 +0800

    Increase environment variable substitution

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,11 +2,19 @@
 
 chown ${APP_RUN_PUID}:${APP_RUN_PGID} -R ${CONTAINER_CODE_PATH}
 
-NGINX_RUN_CONFIG=${NGINX_CONFIG}/${RUN_ENV}/nginx.conf
+NGINX_BUILD_CONFIG=${NGINX_CONFIG}/${RUN_ENV}/nginx.conf
 
-if [ ! -f "${NGINX_RUN_CONFIG}" ]; then
-    NGINX_RUN_CONFIG=/etc/nginx/nginx.conf
+if [ ! -f "${NGINX_BUILD_CONFIG}" ]; then
+    NGINX_BUILD_CONFIG=/etc/nginx/nginx.conf
 fi
+
+NGINX_RUN_CONFIG=$(dirname "${NGINX_BUILD_CONFIG}")/nginx-run.conf
+
+cat ${NGINX_BUILD_CONFIG} \
+| sed "s#\${APP_RUN_NAME}#${APP_RUN_NAME}#g" \
+| sed "s#\${PHP_FPM_PORT}#${PHP_FPM_PORT}#g" \
+| sed "s#\${CONTAINER_CODE_PATH}#${CONTAINER_CODE_PATH}#g" \
+> ${NGINX_RUN_CONFIG}
 
 nginx -g "daemon off;" -c ${NGINX_RUN_CONFIG}
 
