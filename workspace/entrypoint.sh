dd286b7e89345865529aee53588b45ee9c609f8d
crcms <crcms@crcms.cn>
2019-01-05 17:03:30 +0800

    workspace config

--- workspace/entrypoint.sh
+++ workspace/entrypoint.sh
@@ -4,12 +4,13 @@
 supervisor_exists=`whereis supervisord`
 if [ "${supervisor_exists}" = 'supervisord: /usr/bin/supervisord' ]; then
     # supervisord
-    /usr/bin/python /usr/bin/supervisord -c ${WORKSPACE_SUPERVISOR_CONFIG} &
+    /usr/bin/python /usr/bin/supervisord -c ${WORKSPACE_CONFIG}/supervisord.conf &
 fi
 
+workspace_crontab_config=${WORKSPACE_CONFIG}/crontab
 #crontab
-if [ -f "/etc/crontab" -a -f "${WORKSPACE_CRONTAB_CONFIG}" ]; then
-    cat ${WORKSPACE_CRONTAB_CONFIG} \
+if [ -f "/etc/crontab" -a -f "${workspace_crontab_config}" ]; then
+    cat ${workspace_crontab_config} \
     | sed "s#\${CONTAINER_CODE_PATH}#${CONTAINER_CODE_PATH}#g" \
     | sed "s#\${APP_RUN_NAME}#${APP_RUN_NAME}#g" > /etc/crontab
 fi
