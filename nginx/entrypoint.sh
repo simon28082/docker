456c9b099c9b1391ce8ef7db97f62232d62c2a09
crcms <crcms@crcms.cn>
2019-01-05 23:05:59 +0800

    vhost

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -8,7 +8,6 @@ if [ ! -f "${nginx_build_config}" ]; then
     nginx_build_config=/etc/nginx/nginx.conf
 fi
 
-#nginx_run_config=$(dirname "${nginx_build_config}")/nginx-run.conf
 nginx_run_config=/var/nginx-run.conf
 
 cat ${nginx_build_config} \
@@ -16,6 +15,22 @@ cat ${nginx_build_config} \
 | sed "s#\${PHP_FPM_PORT}#${PHP_FPM_PORT}#g" \
 | sed "s#\${CONTAINER_CODE_PATH}#${CONTAINER_CODE_PATH}#g" \
 > ${nginx_run_config}
+
+# vhost conf replace
+if [ -d "${NGINX_CONFIG}/vhosts" ]; then
+    mkdir -p /var/vhosts
+    for current_file in $(ls "${NGINX_CONFIG}/vhosts")
+    do
+        if test -f "${NGINX_CONFIG}/vhosts/${current_file}"
+        then
+            cat "${NGINX_CONFIG}/vhosts/${current_file}" \
+            | sed "s#\${APP_RUN_NAME}#${APP_RUN_NAME}#g" \
+            | sed "s#\${PHP_FPM_PORT}#${PHP_FPM_PORT}#g" \
+            | sed "s#\${CONTAINER_CODE_PATH}#${CONTAINER_CODE_PATH}#g" \
+            > "/var/vhosts/${current_file}"
+        fi
+    done
+fi
 
 nginx -g "daemon off;" -c ${nginx_run_config}
 
