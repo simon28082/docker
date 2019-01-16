e55ffb5b4b1c381c57216f624c7e6e99c18d9131
crcms <crcms@crcms.cn>
2019-01-16 13:23:58 +0800

    change and fix new user and group update

--- php-fpm/entrypoint.sh
+++ php-fpm/entrypoint.sh
@@ -1,24 +1,35 @@
 #!/usr/bin/env bash
 
 #### functions ####
-
+default_user='crcms'
+default_group='crcms'
+default_user_id=1000
+default_group_gid=1000
 # new environment variable user definition
 createRunUserAndGroupIfNotExists(){
     local new_user=${1}
     local new_group=${2}
     local new_user_id=${3}
-    local new_user_gid=${4}
+    local new_group_gid=${4}
 
-    #create group if not exists
-    egrep "^${new_group}" /etc/group >& /dev/null
-    if [ $? -eq 1 ]; then
-        groupadd -g ${4} ${2}
+    local group_exists=`cat /etc/group | cut -f 1 -d ':' | grep -w ${new_group} -c`
+    if [ ${group_exists} -le 0 ]; then
+        groupmod -n ${new_group} ${default_group}
     fi
 
-    id ${new_user} >& /dev/null
-    if [ $? -eq 1 ]; then
-       useradd -u ${3} -g ${4} ${1} && \
-       usermod -s /sbin/nologin ${1}
+    local group_gid_exists=`cat /etc/group | cut -f 3 -d ':' | grep -w ${new_group_gid} -c`
+    if [ ${group_gid_exists} -le 0 ]; then
+        groupmod -g ${new_group_gid} ${new_group}
+    fi
+
+    local user_exists=`cat /etc/passwd | cut -f 1 -d ':' | grep -w ${new_user} -c`
+    if [ ${user_exists} -le 0 ]; then
+        usermod -l ${new_user} -d /home/${new_user} -m ${default_user}
+    fi
+
+    local user_id_exists=`cat /etc/passwd | cut -f 3 -d ':' | grep -w ${new_user_id} -c`
+    if [ ${user_id_exists} -le 0 ]; then
+        usermod -u ${new_user_id} ${new_user}
     fi
 }
 
@@ -30,11 +41,11 @@ autoMount(){
     fi
 }
 
-# create user
+# change user and group
 createRunUserAndGroupIfNotExists ${APP_RUN_NAME} ${APP_RUN_GROUP} ${APP_RUN_PUID} ${APP_RUN_PGID}
 
 #chown
-chown ${APP_RUN_NAME}:${APP_RUN_GROUP} -R ${CONTAINER_CODE_PATH}
+chown ${APP_RUN_PUID}:${APP_RUN_PGID} -R ${CONTAINER_CODE_PATH}
 
 # mount
 autoMount ${PHP_FPM_CONFIG}/mount.sh
