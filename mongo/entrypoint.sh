093c5cf7f60853b604f47e3dfe15dc2da6b30433
crcms <crcms@crcms.cn>
2019-01-16 13:59:27 +0800

    fix

--- mongo/entrypoint.sh
+++ mongo/entrypoint.sh
@@ -12,22 +12,22 @@ createRunUserAndGroupIfNotExists(){
     local new_user_id=${3}
     local new_group_gid=${4}
 
-    local group_exists=`cat /etc/group | cut -f 1 -d ':' | grep -w ${new_group} -c`
+    local group_exists=`cat /etc/group | cut -f 1 -d ':' | grep -w "^${new_group}$" -c`
     if [ ${group_exists} -le 0 ]; then
         groupmod -n ${new_group} ${default_group}
     fi
 
-    local group_gid_exists=`cat /etc/group | cut -f 3 -d ':' | grep -w ${new_group_gid} -c`
+    local group_gid_exists=`cat /etc/group | cut -f 3 -d ':' | grep -w "^${new_group_gid}$" -c`
     if [ ${group_gid_exists} -le 0 ]; then
         groupmod -g ${new_group_gid} ${new_group}
     fi
 
-    local user_exists=`cat /etc/passwd | cut -f 1 -d ':' | grep -w ${new_user} -c`
+    local user_exists=`cat /etc/passwd | cut -f 1 -d ':' | grep -w "^${new_user}$" -c`
     if [ ${user_exists} -le 0 ]; then
         usermod -l ${new_user} -d /home/${new_user} -m ${default_user}
     fi
 
-    local user_id_exists=`cat /etc/passwd | cut -f 3 -d ':' | grep -w ${new_user_id} -c`
+    local user_id_exists=`cat /etc/passwd | cut -f 3 -d ':' | grep -w "^${new_user_id}$" -c`
     if [ ${user_id_exists} -le 0 ]; then
         usermod -u ${new_user_id} ${new_user}
     fi
